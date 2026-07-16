2ca693d6160c2e8badac4dbf042e7d4c398b0554
Basem <basem.sofan@macegroup.com>
2026-07-16 11:16:01 +0300

    clean: reset main branch for rebuild from scratch

--- entrypoints/sidepanel/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { ErrorBoundary } from '@/components/ErrorBoundary';
-import { App } from './App';
-import '@/styles/tailwind.css';
-
-createRoot(document.getElementById('app')!).render(<ErrorBoundary><App /></ErrorBoundary>);
